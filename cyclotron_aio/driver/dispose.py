d6ca6b321dc333df91ec0915a39c97dabf378b6f
Romain Picard <romain.picard@oakbits.com>
2018-06-27 17:08:01 +0200

    Dispose: Handle errors and completion

--- cyclotron_aio/driver/dispose.py
+++ cyclotron_aio/driver/dispose.py
@@ -4,28 +4,30 @@ import asyncio
 
 Sink = namedtuple('Sink', ['dispose'])
 
+
 def make_driver(loop=None):
     ''' Returns a dispose driver function. The optional loop argument can be
     provided to use the driver in another loop than the default one.
     '''
 
-    def dispose(i):
-        if i is not True:
-            return
-
+    def dispose(i = None):
         if loop is not None:
             loop.stop()
         else:
             asyncio.get_event_loop().stop()
 
     def driver(sink):
-        ''' The dispose driver stops the asyncio event loop as soon as a True
-        event is received on the dispose stream.
+        ''' The dispose driver stops the asyncio event loop as soon as an
+        event is received on the dispose stream or when it completes (both
+        in case of success or error).
 
         arguments:
         - sink: A DisposeSink object.
         '''
-        sink.dispose.subscribe(lambda i: dispose(i))
+        sink.dispose.subscribe(
+            on_next=dispose,
+            on_error=dispose,
+            on_completed=dispose)
         return None
 
     return Component(call=driver, input=Sink)
