03b92de6351549a9a7fd470b3e269690c9b31a04
Romain Picard <romain.picard@oakbits.com>
2018-09-05 11:17:28 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name="cyclotron-aio",
-    version='0.4.0',
+    version='0.5.0',
     url='https://github.com/MainRo/cyclotron-aio.git',
     license='MIT',
     description="Asynchronous IO drivers for cyclotron",
